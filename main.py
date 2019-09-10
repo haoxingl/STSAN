571d69fe820afd312278a68bb0c906ff8739d2ce
starkhxl <mb85410@umac.mo>
2019-09-10 14:22:11 +0800

    add gpu setting to main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+gpu_id = "0, 1, 2, 3, 4, 5, 6, 7"
+os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
+
 from train_stream_t import main as main_t
 from train_ST_SAN import main as main_stsan
 
