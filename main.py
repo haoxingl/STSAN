2196104a4a1727ba077092ff54185260bcb7cd61
starkhxl <mb85410@umac.mo>
2019-09-20 15:07:52 +0800

    combine the trainings into one .py file

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 """ 'taxi' or 'bike' dataset """
-dataset = 'taxi'
+dataset = 'bike'
 print("Dataset chosen: {}".format(dataset))
-
-from train_stream_t import main as main_t
-from train_ST_SAN import main as main_stsan
-
 assert dataset == 'taxi' or dataset == 'bike'
 
+from train_models import train_stream_t, train_st_san
+
 if __name__ == "__main__":
-    for index in range(0, 1):
+    for index in range(1, 2):
         model_index = dataset + '_{}'.format(index)
         print("\nStrat training Stream-T...\n")
-        main_t(model_index, dataset)
+        train_stream_t(model_index, dataset)
         print("\nStrat training ST-SAN...\n")
-        main_stsan(model_index, dataset)
+        train_st_san(model_index, dataset)
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-
 import data_loader as dl
 
 
