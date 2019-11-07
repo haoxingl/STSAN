a4d7730e5babd90c8446efef2f6427e8a23cb388
starkhxl <mb85410@umac.mo>
2019-11-07 20:03:25 +0800

    update reshuffle helper

--- utils/ReshuffleHelper.py
+++ utils/ReshuffleHelper.py
@@ -1,17 +1,17 @@
 class ReshuffleHelper:
-    def __init__(self, es_patience=15, thres=[0.8, 1.3, 1.7]):
+    def __init__(self, es_patience=15, thresholds=[0.8, 1.3, 1.7]):
         self.es_patience = es_patience
-        self.thres = thres
-        self.flags = [False for _ in range(len(thres))]
+        self.thresholds = thresholds
+        self.flags = [False for _ in range(len(thresholds))]
 
     def check(self, epoch):
         epoch += 1
         for (index, flag) in enumerate(self.flags):
             if not flag:
-                if epoch == int(self.es_patience * self.thres[index]):
+                if epoch == int(self.es_patience * self.thresholds[index]):
                     self.flags[index] = True
-                    if index < len(self.thres) - 1:
-                        self.thres[index + 1] += self.thres[index]
+                    if index < len(self.thresholds) - 1:
+                        self.thresholds[index + 1] += self.thresholds[index]
                     print("Reshuffling...\n")
                     return True
                 else:
